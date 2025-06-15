3b7877d355c8ce5f4e3aff58e0902741d44844e9
adasken <duckhang.quach@gmail.com>
2025-06-15 19:36:42 +1000

    141

--- push.sh
+++ push.sh
@@ -1,3 +1,12 @@
+# Add all changes
 git add .
-git commit -m "update"
+
+# Use the first argument as the commit message, or "bug fix" if no argument is provided
+if [ -z "$1" ]; then
+  git commit -m "bug fix"
+else
+  git commit -m "$1"
+fi
+
+# Push the changes
 git push
